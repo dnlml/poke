179a1ce308573dc7f7f72e1bebb3ab9e32b32405
dnlml <info@danielmeli.com>
2022-03-11 17:07:15 +0100

    Setting up basic layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about" class:active={$page.url.pathname == '/about'}>About</a>
+</nav>
+<ul>
+  <li>{$page.url.pathname}</li>
+</ul>
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <h1>Welcome to SvelteKit <span>ciao</span></h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/about">About</a>
 
 <style lang="scss">
   h1 {
