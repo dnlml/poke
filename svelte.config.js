52f6e42b9d135805dd1c132a3f3d7ea25ddc6d27
dnlml <info@danielmeli.com>
2022-03-17 17:03:38 +0100

    Adding build step, video for testing, favicon and adding tests

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import autoprefixer from 'autoprefixer';
 import sveltePreprocess from 'svelte-preprocess';
 
@@ -13,7 +13,12 @@ const config = {
   }),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    })
   }
 };
 
